cca4253cba2414e5dcaed76d7254b59ca1a502a2
Justin James <justin.james@yale.edu>
2020-05-10 04:33:35 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,15 +1,15 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
  
-  array.count do | element |
+  array.count { | element |
     element.class == String
-  end 
+  }
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 
-  array.count do | element |
+  array.count { | element |
    element == ""
-  end 
+  } 
 end
